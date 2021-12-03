4909d46b6d9c6e2dcb9f2e30bce988f4d198e345
sweetzachary <a.ntigr16@gmail.com>
2021-12-03 14:19:09 +0200

    Implement substring counting method

---
+++ sub_strings.rb
@@ -0,0 +1,11 @@
+def count_word(s, word)
+    (s.length - s.gsub(word, '').length)/word.length
+end
+
+def sub_strings(string, dict)
+    dict.reduce({}) {|count, word| count.merge({word => count_word(string.downcase, word)})}.filter {|k, v| v != 0}
+end
+
+dictionary = ["below","down","go","going","horn",
+              "how","howdy","it","i","low","own","part","partner","sit"]
+puts sub_strings "Howdy partner, sit down! How's it going?", dictionary
